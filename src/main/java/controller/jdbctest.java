d1d73b4fd7030eb7d1c42d1c14288807e4bfb825
shilina <shiln@miqtech.com>
2016-09-09 18:09:59 +0800

    数据库连接新加

---
+++ src/main/java/controller/jdbctest.java
@@ -0,0 +1,38 @@
+package controller;
+
+import java.beans.Statement;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class jdbctest {
+
+	public static void main(String[] args) throws SQLException {
+		ApplicationContext ctx=new ClassPathXmlApplicationContext("applicationContext.xml");  
+		 
+		   DataSource dataSource=ctx.getBean("dataSource",DataSource.class);
+		 
+		     String sql="select * from user";   
+		 
+		  Connection connection=dataSource.getConnection(); 
+		 
+		  Statement stm=(Statement) connection.createStatement();   
+		 
+		  ResultSet rs=((java.sql.Statement) stm).executeQuery(sql); 
+		 
+		    while(rs.next())    
+		 
+		{       System.out.println("用户名为:"); 
+		 
+		     System.out.println(rs.getString(2)); 
+		 
+		    }                  
+
+	}
+
+}
